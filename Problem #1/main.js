72c55330f82a685ba60f77a4c92e14f6a5689bf6
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-06-22 22:10:51 +0700

    Program now can display input in 7segment format

--- Problem #1/main.js
+++ Problem #1/main.js
@@ -12,7 +12,7 @@ const receiveUserInput = (userInput) => {
     const inputText = prompt('input: '); // receive input from keyboard
 
     // verify user's intput pattern  
-    if(regx.test(inputText) && isValid) {
+    if(regx.test(inputText)) {
         let inputArr = inputText.split(':');
         // check if minute and second are in valid range
         const isValid = (Number(inputArr[1]) < 60) && (Number(inputArr[2]) < 60);
@@ -43,46 +43,151 @@ const convertToDigit = (time) => {
     return digitArray;
 }
 
+const updateTime = (time) => {
+
+}
+
 const makeSevenSeg = (number) => {
-    const sevenSegment = new Array[7];
+    // array of string
+    const sevenSegment = new Array(7);
 
     switch(number) {
         case 0:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '  ';
+            sevenSegment[2] = '__';
+            for(let i = 3; i <= 6; i++) {
+                sevenSegment[i] = '|';
+            }
+            break;
         case 1:
-
+            sevenSegment[0] = '  ';
+            sevenSegment[1] = '  ';
+            sevenSegment[2] = '  ';
+            sevenSegment[3] = ' ';
+            sevenSegment[4] = ' ';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = '|';
+            break;
         case 2:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '__';
+            sevenSegment[3] = ' ';
+            sevenSegment[4] = '|';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = ' ';
+            break;
         case 3:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '__';
+            sevenSegment[3] = ' ';
+            sevenSegment[4] = ' ';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = '|';
+            break;
         case 4:
-
+            sevenSegment[0] = '  ';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '  ';
+            sevenSegment[3] = '|';
+            sevenSegment[4] = ' ';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = '|';
+            break;
         case 5:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '__';
+            sevenSegment[3] = '|';
+            sevenSegment[4] = ' ';
+            sevenSegment[5] = ' ';
+            sevenSegment[6] = '|';
+            break;
         case 6:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '__';
+            sevenSegment[3] = '|';
+            sevenSegment[4] = '|';
+            sevenSegment[5] = ' ';
+            sevenSegment[6] = '|';
+            break;
         case 7:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '  ';
+            sevenSegment[2] = '  ';
+            sevenSegment[3] = ' ';
+            sevenSegment[4] = ' ';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = '|';
+            break;
         case 8:
-
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '__';
+            sevenSegment[3] = '|';
+            sevenSegment[4] = '|';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = '|';
+            break;
         case 9:
+            sevenSegment[0] = '__';
+            sevenSegment[1] = '__';
+            sevenSegment[2] = '__';
+            sevenSegment[3] = '|';
+            sevenSegment[4] = ' ';
+            sevenSegment[5] = '|';
+            sevenSegment[6] = '|';
+            break;
     }
     return sevenSegment;
 }
 
 const displaySevenSeg = (digitArray) => {
 
-    const displayTime = digitArray.map(makeSevenSeg(number));
+    //displayTime is 2d array of string: row is number index, column is segment index
+    const displayTime = digitArray.map(makeSevenSeg);
     //display
+    let firstLine = ' ', secondLine = '', thirdLine = '';
+    for(let i = 0; i < 6; i++) {
+        firstLine += ` ${displayTime[i][0]} `;
+
+        if((i + 1) % 2 === 0 && i < 5) {
+            firstLine += "   ";
+        }
+        else {
+            firstLine += " ";
+        }
+    }
+
+    for(let i = 0; i < 6; i++) {
+        secondLine += displayTime[i][3] + displayTime[i][1] + displayTime[i][5];
+        if((i + 1) % 2 === 0 && i < 5) {
+            secondLine += " · ";
+        }
+        else {
+            secondLine += " ";
+        }
+    }
+
+    for(let i = 0; i < 6; i++) {
+        thirdLine += displayTime[i][4] + displayTime[i][2] + displayTime[i][6];
+        if((i + 1) % 2 === 0 && i < 5) {
+            thirdLine += " · ";
+        }
+        else {
+            thirdLine += " ";
+        }
+    }
+    console.log(firstLine, '\n', secondLine, '\n', thirdLine);
+
 }
 
 const displayNone = () => {
     console.log('\n          ·           ·');
     console.log(' __   __  ·  __   __  ·  __  __');
-}
-
-const updateTime = (time) => {
-
 }
 
 const main = () => {
@@ -91,8 +196,7 @@ const main = () => {
     const response = receiveUserInput(startTime);
     if(response) {
         const digitArray = convertToDigit(startTime);
-        console.log(digitArray);
-        //displaySevenSeg(digitArray);
+        displaySevenSeg(digitArray);
     }
     else {
         displayNone();
