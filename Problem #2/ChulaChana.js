9ab8eff180e40f0cb025344919511332996f96d8
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-06-23 01:05:18 +0700

    Outline problem

---
+++ Problem #2/ChulaChana.js
@@ -0,0 +1,27 @@
+const utils = require('./Utils');
+
+const areaList = ['Mahamakut Building', 'Sara Phra Kaew', 'CU Sport Complex', 'Sanum Juub', 'Samyan Mitr Town', 'Central library'];
+
+const getPopulation = () => {
+
+}
+
+const getUserList = () => {
+
+}
+
+const checkIn = (userList, populationData) => {
+
+}
+
+const checkOut = (userList, populationData) => {
+
+}
+
+const displayPopulation = (populationData) => {
+
+}
+
+const saveData = (userList, populationData) => {
+
+}
---
+++ Problem #2/main.js
@@ -0,0 +1,35 @@
+const utils = require('./Utils');
+const CC = require('./ChulaChana');
+
+// Prompt to user
+console.log("Welcome to Chula Chana!!!");
+console.log("Available commands:\n", " 1.Check in user\n", " 2.Check out user\n",
+    " 3.Show population\n", " 4.quit");
+
+// Load data into memory
+const userList = cc.getUserList();
+const populationData = cc.getPopulation();
+
+let finish = false; // for exit the program
+do {
+    const userInput = utils.receiveInputNumber("Please input any number (1-4): ", 4);
+
+    switch(userInput) {
+        case 1:
+            CC.checkIn(userList, populationData);
+            break;
+        case 2:
+            CC.checkOut(userList, populationData);
+            break;
+        case 3:
+            displayPopulation(populationData);
+            break;
+        case 4:
+            finish = true;
+            break;
+    }
+
+} while(!finish);
+
+cc.saveData(userList, populationData);
+console.log("closing program....");
