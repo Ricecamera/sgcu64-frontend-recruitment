fbd9d2b4e6a7f0fea56db0ecbea07c3c56a65778
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-06-24 02:05:30 +0700

    Add some comment to Utils.js

--- Problem #2/Utils.js
+++ Problem #2/Utils.js
@@ -3,12 +3,14 @@ const fs = require("fs");
 const csv = require('csv-parser');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+// recieve user type from keyborad
 const receiveInputText = (inputPrompt) => {
     const inputText = prompt(inputPrompt);
     return inputText;
 }
 
-const receiveInputNumber = (inputPrompt, maxNum) => {
+// recieve number from keyboard
+const receiveInputNumber = (inputPrompt, maxNum = 1) => {
     let input;
     let fail;
     do {
@@ -16,9 +18,11 @@ const receiveInputNumber = (inputPrompt, maxNum) => {
         input = prompt(inputPrompt);
 
         if(!isNaN(input) && (Number(input) > 0 && Number(input) <= maxNum)) {
+            // accept input when user type is number and value is between 1 and maxNum
             input = Number(input);
         }
         else {
+            // reject input
             console.log("invalid input please try again!");
             fail = true;
         }
@@ -27,15 +31,16 @@ const receiveInputNumber = (inputPrompt, maxNum) => {
     return input;
 }
 
+// read csv file asynchronously
 const readCSVfile = (filePath) => {
     let result = [];
     return new Promise((resolve, reject) => {
         fs.createReadStream(filePath).pipe(csv({ headers: true }))
             .on('data', (data) => result.push(data))
             .on('end', () => {
-                resolve(result);
+                resolve(result); // success, return data in array
             })
-            .on('error', reject)
+            .on('error', reject) // error, can't open the file
     });
 }
 
